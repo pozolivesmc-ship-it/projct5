f5a6ab3af529554d00e09670289bb5032c9c6d0e
giovanni232024S <giovanni23@vt.edu>
2025-11-20 14:34:53 -0500

    Finished SkipList and added Bintree logic

---
+++ src/Bintree.java
@@ -0,0 +1,56 @@
+
+/**
+ * This is the Bintree class 
+ * @author {Giovanni Garcia}
+ * @version {11.20.2025}
+ */
+public class Bintree {
+    
+    private BinNode root;
+    
+    private static final int X = 0;
+    private static final int Y = 0;
+    private static final int Z = 0;
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT = 1024;
+    private static final int DEPTH = 1024;
+    
+    /**
+     * This is the constructor for Bintree
+     * The root is an EmptyNode, depth = 0
+     */
+    public Bintree() 
+    {
+        root = new EmptyNode();
+    }
+    
+    private interface BinNode
+    {
+        String print(int x, int y, int z, int w, int h, int d, int depth);
+        int countNodes();
+    }
+    
+    private static class EmptyNode implements BinNode
+    {
+        public String print(int x, int y, int z, int w, int h, int d, int depth)
+        {
+            return "E (" + x + ", " + y + ", " + z + ", " 
+                   + w + ", " + h + ", " + d + ") " + depth + "\r\n";
+        }
+        public int countNodes()
+        {
+            return 1;
+        }
+    }
+    /**
+     * This is the printTree method
+     * @return String for the tree
+     */
+    public String printTree() 
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.print(X, Y, Z, WIDTH, HEIGHT, DEPTH, 0));
+        sb.append(root.countNodes()).append(" Bintree nodes printed\r\n");
+        return sb.toString();
+    }
+}    
--- src/SkipList.java
+++ src/SkipList.java
@@ -4,12 +4,10 @@ import java.util.Random;
  * This is the SkipList class 
  * @author {Giovanni Garcia}
  * @version {11.16.2025}
- * @param <N> for Name 
- * @param <O> for object
  */
-public class SkipList <N extends Comparable<N>, O>{
+public class SkipList {
 
-    private static final int HIGHEST_LEVEL = 32;
+    private static final int HIGHEST_LEVEL = 16;
     private SkipNode head;
     private int level;
     private Random rnd;
@@ -17,26 +15,123 @@ public class SkipList <N extends Comparable<N>, O>{
     /**
      * This is the inner SkipNode class
      */
-    private class SkipNode
+    private static class SkipNode
     {
-        N name;
-        O object;
+        String key ;
+        AirObject value;
         SkipNode[] forward;
-        
-        public SkipNode(N name, O object, int nodeLevel)
+        /**
+         * This is the constructor for SkipNode
+         * @param key is the name
+         * @param value is the AirObject
+         * @param level is the level
+         */
+        public SkipNode(String key, AirObject value, int level)
         {
-            this.name = name;
-            this.object = object;
-            //this.forward = new SkipNode[nodeLevel + 1];
+            this.key = key;
+            this.value = value;
+            forward = new SkipNode[level + 1];
         }    
     }
         /**
          * This is the SkipList class constructor 
          * @param rnd is the random object
          */
-        public SkipList(Random rnd)
+        public SkipList(Random rand)
         {
-            this.rnd = rnd;
-            
+            if (rand == null)
+            {
+                rnd = new Random();
+            }
+            else
+            {
+                rnd = rand;
+            }
+            level = 0;
+            head = new SkipNode(null, null, HIGHEST_LEVEL);
+        }
+        
+        /**
+         * This is the find method
+         * @param name is the name we're looking for
+         * @return AirObject found
+         */
+        public AirObject find(String name)
+        {
+            //Start from the head
+            SkipNode start = head;
+            //Start comparing from the highest level 
+            for (int i = level; i >= 0; i--)
+            {
+                while (start.forward[i] != null &&
+                       start.forward[i].key.compareTo(name) < 0)
+                {
+                    start = start.forward[i];
+                }
+            }
+            //Move to it if exists
+            start = start.forward[0];
+            //If found and names match
+            if (start != null && start.key.equals(name))
+            {
+                return start.value;
+            }
+            //If not found
+            return null;
+        }
+        /**
+         * Picks randoms level 
+         * @return int for level
+         */
+        public int randomLevel() 
+        {
+            int rndLevel = 0;
+            while (Math.abs(rnd.nextInt()) % 2 == 0 && rndLevel < HIGHEST_LEVEL)
+            {
+                rndLevel++;
+            }
+            return rndLevel;
+        }
+        
+        public void insert(String name, AirObject object)
+        {
+            SkipNode[] update = new SkipNode[HIGHEST_LEVEL + 1];
+            //Start from the head
+            SkipNode start = head;
+            //Start comparing from the highest level 
+            for (int i = level; i >= 0; i--)
+            {
+                while (start.forward[i] != null &&
+                       start.forward[i].key.compareTo(name) < 0)
+                {
+                    start = start.forward[i];
+                }
+                //Add end of level i
+                update[i] = start; 
+            }
+            //Move to it if exists
+            start = start.forward[0];
+            //If found and names match replace
+            if (start != null && start.key.equals(name))
+            {
+                start.value = object;
+                return;
+            }
+            int rndLevel = randomLevel();
+            if (rndLevel > level)
+            {
+                for (int i = level + 1; i <= rndLevel; i++)
+                {
+                    update[i] = head;
+                }
+                level = rndLevel;
+            }
+            start = new SkipNode(name, object, rndLevel);
+            //Splice into list for every level
+            for (int i = 0; i <= rndLevel; i++)
+            {
+                start.forward[i] = update[i].forward[i];
+                update[i].forward[i] = start;
+            }
         }
     }
