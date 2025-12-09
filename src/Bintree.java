08cc656a87c61251ce3e546c9048ebed08f932de
giovanni232024S <giovanni23@vt.edu>
2025-12-08 20:06:50 -0500

    Bintree design

--- src/Bintree.java
+++ src/Bintree.java
@@ -7,7 +7,8 @@
 public class Bintree {
     
     private BinNode root;
-    
+    private static final EmptyNode FLYWEIGHT = new EmptyNode();
+    private static final int LEAF_MAX = 4;
     private static final int X = 0;
     private static final int Y = 0;
     private static final int Z = 0;
@@ -21,41 +22,19 @@ public class Bintree {
      */
     public Bintree() 
     {
-        root = new EmptyNode();
-    }
-    
-    /**
-     * This is the interface for BinNode 
-     * since other nodes will be implementing them
-     */
-    private interface BinNode
-    {
-        String print(int x, int y, int z, int w, int h, int d, int depth);
-        int countNodes();
-    }
-    
-    /**
-     * This is the EmptyNode class
-     */
-    private static class EmptyNode implements BinNode
-    {
-        public String print(int x, int y, int z, int w, int h, int d, int depth)
-        {
-            return "E (" + x + ", " + y + ", " + z + ", " 
-                   + w + ", " + h + ", " + d + ") " + depth + "\r\n";
-        }
-        public int countNodes()
-        {
-            return 1;
-        }
+        root = FLYWEIGHT; 
     }
     /**
      * This is the insert method for Bintree
-     * @param object is the object inserted
+     * @param obj is the object inserted
      */
-    public void insert(AirObject object)
+    public void insert(AirObject obj)
     {
-        //Finish this later
+        if (obj == null)
+        {
+            return;
+        }
+        root = root.insert(obj, X, Y, Z, WIDTH, HEIGHT, DEPTH, 0);
     }
     /**
      * This is the printTree method
@@ -64,8 +43,116 @@ public class Bintree {
     public String printTree() 
     {
         StringBuilder sb = new StringBuilder();
-        sb.append(root.print(X, Y, Z, WIDTH, HEIGHT, DEPTH, 0));
+        root.print(sb, X, Y, Z, WIDTH, HEIGHT, DEPTH, 0);
         sb.append(root.countNodes()).append(" Bintree nodes printed\r\n");
         return sb.toString();
+    }
+    private static void spacing(StringBuilder sb, int depth)
+    {
+        for (int i = 0; i < depth; i++)
+        {
+            sb.append("  ");
+        }
+    }
+    /**
+     * This is the interface for BinNode 
+     */
+    private interface BinNode
+    {
+        void print(StringBuilder sb, int x, int y, int z, int w, int h, int d, int depth);
+        int countNodes();
+        BinNode insert(AirObject obj, int x, int y, int z, int w, int h, int d, int depth);
+    }
+    
+    /**
+     * This is the EmptyNode class
+     */
+    private static class EmptyNode implements BinNode {
+        public void print(StringBuilder sb, int x, int y, int z, int w, int h, int d, int depth)
+        {
+            spacing(sb, depth);
+            sb.append("E (").append(x).append(", ").append(y).append(", ");
+            sb.append(z).append(", ").append(w).append(", ").append(h).append(", ");
+            sb.append(d).append(") ").append(depth).append("\r\n");
+        }
+        public int countNodes()
+        {
+            return 1;
+        }
+        public BinNode insert(AirObject obj, int x, int y, int z, int w, int h, int d, int depth)
+        {
+            LeafNode leaf = new LeafNode();
+            leaf.addObject(obj);
+            return leaf;
+        }
+    }
+    /**
+     * This is the LeafNode class
+     */
+    private static class LeafNode implements BinNode {
+        private AirObject[] objects;
+        private int size;
+        
+        public LeafNode() {
+            objects = new AirObject[LEAF_MAX];
+            size = 0;
+        }
+        
+        public void addObject(AirObject obj)
+        {
+            //Finish this
+        }
+        public void print(StringBuilder sb, int x, int y, int z, int w, int h, int d, int depth)
+        {
+            spacing(sb, depth);
+            sb.append("Leaf with ").append(size).append(" objects (");
+            sb.append(x).append(", ").append(y).append(", ");
+            sb.append(z).append(", ").append(w).append(", ").append(h).append(", ");
+            sb.append(d).append(") ").append(depth).append("\r\n");
+            for (int i = 0; i < size; i++)
+            {
+                spacing(sb, depth + 1);
+                sb.append("(").append(objects[i].toString()).append(")\r\n");
+            }
+        }
+        public int countNodes()
+        {
+            return 1;
+        }
+        public BinNode insert(AirObject obj, int x, int y, int z, int w, int h, int d, int depth)
+        {
+            //Finish code
+            return this;
+        }
+    }
+    /**
+     * This is the InternalNode class
+     */
+    private static class InternalNode implements BinNode {
+        private BinNode left;
+        private BinNode right;
+        
+        public InternalNode() {
+            left = FLYWEIGHT;
+            right = FLYWEIGHT;
+        }
+        public void print(StringBuilder sb, int x, int y, int z, int w, int h, int d, int depth)
+        {
+            spacing(sb, depth);
+            sb.append("I (");
+            sb.append(x).append(", ").append(y).append(", ");
+            sb.append(z).append(", ").append(w).append(", ").append(h).append(", ");
+            sb.append(d).append(") ").append(depth).append("\r\n");
+            //Finish this
+        }
+        public int countNodes()
+        {
+            return 1 + left.countNodes() + right.countNodes();
+        }
+        public BinNode insert(AirObject obj, int x, int y, int z, int w, int h, int d, int depth)
+        {
+            //Finish this
+            return this;
+        }
     }
 }    
